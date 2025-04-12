3655dd5e5593b35c4d4588016bea240d9c5d73d4
박성현 <sunghyeon5307@gmail.com>
2025-04-13 01:20:39 +0900

    최최종

--- testimg.py
+++ testimg.py
@@ -12,12 +12,10 @@ result1 = model(image)
 for r in result1:
     for box in r.boxes:
         x1, y1, x2, y2 = map(int, box.xyxy[0])
-        cropped = image[y1:y2, x1:x2]  # numpy 이미지 자르기
+        cropped = image[y1:y2, x1:x2]  
 
-        # 2단계: 잘라낸 영역에서 색상 탐지
         result2 = model2(cropped)
 
-        # 결과 시각화 (box + label 포함)
         result_img = result2[0].plot()
         cv2.imshow("Detected Traffic Light Color", result_img)
         cv2.waitKey(0)
