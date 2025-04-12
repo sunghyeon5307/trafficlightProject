93df39157ce7d35e171643261c1b9f0d5d322962
박성현 <sunghyeon5307@gmail.com>
2025-04-13 01:25:18 +0900

    수수정

--- performance.py
+++ performance.py
@@ -4,7 +4,7 @@ model = YOLO('/Users/bagseonghyeon/Documents/traffic_light/color_best.pt')
 
 metrics = model.val(data='/Users/bagseonghyeon/Documents/traffic_light/augmented/data.yaml')  
 
-# print(metrics)
+print(metrics)
 
 print(f"Precision: {metrics.box.map50:.4f}")  
 print(f"Recall: {metrics.box.map50:.4f}")     
